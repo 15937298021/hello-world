e23d58746c6c6d8c493fcf535f4f329ccf482486
15937298021 <55096998+15937298021@users.noreply.github.com>
2019-10-09 23:22:15 +0800

    课件代码

---
+++ Main.java
@@ -0,0 +1,11 @@
+import course. Course;
+import student. Student; 
+import teacher. Teacher;
+public class Main
+{
+public static void main(String[] args) {
+Course cs = new Course () ;
+Student st = new Student();
+Teacher te = new Teacher ();
+}
+}
