d314287305e4e94d911ff0faec8b64cc0df8565a
15937298021 <55096998+15937298021@users.noreply.github.com>
2019-12-05 22:58:23 +0800

    重构pbl作业----分解并重组statement()方法第三步
    搬移金额计算代码，并去除statement方法中无用的变量

--- pbl作业/Customer.java
+++ pbl作业/Customer.java
@@ -27,7 +27,7 @@ public class Customer {
         while (rentals.hasMoreElements()) {
             double thisAmount = 0;
             Rental each = (Rental) rentals.nextElement();
-thisAmount = amountFor(each);
+thisAmount = each.getCharge();
    
 //add frequent renter points
             frequentRenterPoints++;
@@ -35,8 +35,8 @@ thisAmount = amountFor(each);
             if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1)
                 frequentRenterPoints++;
 //show figures for this rental
-            result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(thisAmount) + "\n";
-            totalAmount += thisAmount;
+            result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(each.getCharge()) + "\n";
+            totalAmount += each.getCharge();
         }
 //add footer lines
         result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
@@ -44,22 +44,5 @@ thisAmount = amountFor(each);
         return result;
     }
 }
-private double amountFor(Rental aRmental){
-double result = 0;
-switch (aRental.getMovie().getPriceCode()) {
-                case Movie.REGULAR:
-                    result += 2;
-                    if (aRental.getDaysRented() > 2)
-                        result += (aRental.getDaysRented() - 2) * 1.5;
-                    break;
-                case Movie.NEW_RELEASE:
-                    result+= aRental.getDaysRented() * 3;
-                    break;
-                case Movie.CHILDRENS:
-                    result += 1.5;
-                    if (aRental.getDaysRented() > 3)
-                        result += (aRental.getDaysRented() - 3) * 1.5;
-                    break;
-}
-return result;
+
 }
